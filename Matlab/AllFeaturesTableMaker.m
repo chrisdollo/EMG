87a4a41ce2418bb31f6ad5c613295bff6881b805
Chris <is>
2025-07-31 10:50:18 -0400

    update

--- Matlab/AllFeaturesTableMaker.m
+++ Matlab/AllFeaturesTableMaker.m
@@ -3,12 +3,15 @@ numberOfParticipants = 3;
 numGestureRepPerPar = 28;
 uniqueGestures = 7;
 numSensors = 24;
-numFeatures = 18;
+% numFeatures = 18;
+
+sample_num = 1500
+
 
 % Create the cell content
-k = cell(numSensors, numFeatures);
+k = cell(sample_num, numSensors);
 finalCellArray = repmat({k}, numberOfParticipants * numGestureRepPerPar, uniqueGestures);  % 84×7 cell array
 
 % Save it to .mat
-fullname = fullfile(pwd, 'gestureTable_clean.mat');
+fullname = fullfile(pwd, 'gestureTable_clean_deep_learning.mat');
 save(fullname, 'finalCellArray');
